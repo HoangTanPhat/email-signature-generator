59df4d9132655f01edfc7354aa1b2966eee8ad34
Hoang Tan Phat <hoangtanphat97.xm@gmail.com>
2022-03-15 18:31:24 +0700

    fix bugs

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -106,15 +106,16 @@ export default function Result({setSuccess}) {
                 style={{
                   borderBottomLeftRadius: "1rem",
                   borderBottomRightRadius: "1rem",
+                  borderColor: "white",
                 }} ref={template1Ref}>
                     <tbody>
                       <tr className="d-flex flex-row flex-wrap">
-                        <td className="d-flex flex-wrap align-items-center justify-content-center p-4 flex-grow-0" style={{width: "30%", justifyContent:"center", alignItems:"center"}}>
+                        <td className="d-flex flex-wrap avatar-wrapper-template p-4" style={{width: "30%", justifyContent:"center", alignItems:"center"}}>
                           <a className="d-block" style={{width: `${imageWidthDynamic}px`, height: `${imageWidthDynamic}px`}}>
                             <img className="rounded-circle" src={userInfo.image} style={{maxWidth: "100%", height: `${imageWidthDynamic}px`, objectFit:"cover", width:`${imageWidthDynamic}px`}} />  
                           </a>
                         </td>
-                        <td className="py-3 px-4 flex-grow-1">
+                        <td className="py-3 px-4" style={{width: "70%"}}>
                             <h2 onClick={() => chooseField('fieldName')} className="fw-bold" style={{margin:"0"}}>{userInfo.name}</h2>
                             <hr style={{margin: "0.5rem 0px"}} />
                             <h4 style={{margin: "0p"}} className="fw-bold">{userInfo.position}</h4>
